04ccb569ff8a20672e4b7c7385f70eebd489cee2
kulakovskyi <torchok9@gmail.com>
2023-12-19 12:45:23 +0200

    update dates

--- front/js/main.js
+++ front/js/main.js
@@ -130,10 +130,9 @@ window.addEventListener('orientationchange', () => {
 
 
 
-
 (function () {
     var url = new URL(window.location.href);
-    var params = ['l', 'utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content', 'param1', 'param2'];
+    var params = ['l', 'utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content', 'param1', 'param2', 'param3', 'param4', 'creative_type', 'creative_id'];
     var linkParams = ['affid', 'cpaid']; // ищем в url redirectUrl в url:
 
     if (url.searchParams.has('redirectUrl')) {
@@ -143,15 +142,16 @@ window.addEventListener('orientationchange', () => {
             //если ссылка в ссылка redirectUrl корректная
             localStorage.setItem('redirectUrl', redirectUrl.href); // указываем точкой входа домен с протоколом из redirectUrl
         }
-    } /////////
-
+    }
 
     params.forEach(function (param) {
         if (url.searchParams.has(param)) localStorage.setItem(param, url.searchParams.get(param));
     });
+
     linkParams.forEach(function (linkParam) {
         if (url.searchParams.has(linkParam)) localStorage.setItem(linkParam, url.searchParams.get(linkParam));
     });
+
     window.addEventListener('click', function (e) {
         var link,
             parent = e.target.closest('a');
@@ -160,16 +160,28 @@ window.addEventListener('orientationchange', () => {
             return;
         }
 
-        parent && (e.preventDefault(),
-            localStorage.getItem("redirectUrl")
-                ? link = new URL(localStorage.getItem("redirectUrl"))
-                : (link = new URL(parent.href),
-                    affid = localStorage.getItem('affid'),
-                    cpaid = localStorage.getItem('cpaid'),
-                affid && cpaid && (link.pathname = '/' + affid + '/' + cpaid)), params.forEach(function (param)
-        {
-            url.searchParams.has(param) && link.searchParams.set(param, localStorage.getItem(param));
-        }), document.location.href = link);
+        if (parent) {
+            e.preventDefault();
+            var affid = localStorage.getItem('affid');
+            var cpaid = localStorage.getItem('cpaid');
+
+            if (localStorage.getItem("redirectUrl")) {
+                link = new URL(localStorage.getItem("redirectUrl"));
+            } else {
+                link = new URL(parent.href);
+                if (affid && cpaid) {
+                    link.pathname = '/' + affid + '/' + cpaid;
+                }
+            }
+
+            params.forEach(function (param) {
+                if (url.searchParams.has(param)) {
+                    link.searchParams.set(param, localStorage.getItem(param));
+                }
+            });
+
+            document.location.href = link;
+        }
     });
 })();
 
